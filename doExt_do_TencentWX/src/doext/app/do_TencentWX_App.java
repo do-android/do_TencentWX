bf5dc6c75ce2e62cac7a83b4fb4872a2f2b05a22
maorui <maoruiily@163.com>
2015-08-13 15:34:12 +0800

    update

--- doExt_do_TencentWX/src/doext/app/do_TencentWX_App.java
+++ doExt_do_TencentWX/src/doext/app/do_TencentWX_App.java
@@ -1,32 +1,38 @@
 package doext.app;
+
 import android.content.Context;
 import core.interfaces.DoIAppDelegate;
 
 /**
  * APP启动的时候会执行onCreate方法；
- *
+ * 
  */
 public class do_TencentWX_App implements DoIAppDelegate {
 
 	private static do_TencentWX_App instance;
-	
-	private do_TencentWX_App(){
-		
+
+	private do_TencentWX_App() {
+
 	}
-	
+
 	public static do_TencentWX_App getInstance() {
-		if(instance == null){
+		if (instance == null) {
 			instance = new do_TencentWX_App();
 		}
 		return instance;
 	}
-	
+
 	@Override
 	public void onCreate(Context context) {
 		// ...do something
 	}
-	
+
 	public String getModuleTypeID() {
 		return "do_TencentWX";
+	}
+
+	@Override
+	public String getTypeID() {
+		return getModuleTypeID();
 	}
 }
